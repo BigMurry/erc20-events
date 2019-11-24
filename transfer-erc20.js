21e12ca36a1768e323db5f939d1f85e241d98d52
BigMurry <hhdiao@siom.ac.cn>
2019-11-24 16:09:24 +0800

    fix: ledger bugs

--- transfer-erc20.js
+++ transfer-erc20.js
@@ -1,7 +1,6 @@
+require('regenerator-runtime'); // ledgerhq issue hotfix
 const ethers = require('ethers');
-const Tx = require('ethereumjs-tx');
-require('regenerator-runtime');
-const Eth = require('@ledgerhq/hw-app-eth');
+const Eth = require('@ledgerhq/hw-app-eth').default;
 const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default;
 const BN = require('bignumber.js');
 
@@ -33,6 +32,10 @@ const USDT_CONF = {
       'block': 6414181, // initial block number for ropsten testnet
       'address': '0x7A434FBdAca6D9Bee580FE2c285c9859BC63FB67'
     },
+    '4': { // rinkeby pow testnet
+      'block': 5472000, // initial block number for rinkebby ropsten testnet
+      'address': '0x7cc3489C1A2887991D443B05a44A2f260998AdaD'
+    },
     '5': { // goerli poa testnet
       'block': 1318894, // initial block number for goerli testnet
       'address': '0xaCCEd4e43D89177de0b77fD4C0f53ac215F02627'
@@ -40,7 +43,14 @@ const USDT_CONF = {
   }
 };
 
-const USDT_TRANSFER_GAS_LIMIT = '0x15f90'; // 90,000 gas limit
+const NETWORKS = {
+  1: 'homestead',
+  3: 'ropsten',
+  4: 'rinkeby',
+  5: 'goerli'
+};
+
+const USDT_TRANSFER_GAS_LIMIT = '0x015f90'; // 90,000 gas limit
 
 function getCalldata(abi, method, args) {
   const fn = new ethers.utils.Interface(abi).functions[method];
@@ -51,55 +61,65 @@ function getCalldata(abi, method, args) {
 }
 
 function getRawTx(to, usdtAmount, nonce, gasGwei, chainId = 1) { // default mainnet
+  // usdt transfer calldata
   const calldata = getCalldata(USDT_CONF.abi, 'transfer', [to, usdtAmount]);
 
   const rawTx = {
-    nonce: '0x' + Number(nonce).toString(16),
-    gasPrice: '0x' + ethers.utils.parseUnits(gasGwei, 'gwei'),
+    nonce: ethers.utils.hexlify('0x' + Number(nonce).toString(16)),
+    gasPrice: ethers.utils.parseUnits(gasGwei + '', 'gwei'),
     gasLimit: USDT_TRANSFER_GAS_LIMIT,
     to: USDT_CONF.networks[chainId].address,
     value: '0x00',
     chainId,
-    r: '0x00',
-    s: '0x00',
-    v: `0x0${chainId}`, // note: https://github.com/LedgerHQ/ledgerjs/issues/43#issuecomment-366984725
-    data: '0x' + calldata
+    data: calldata
   };
-  const txToSign = new Tx(rawTx).serialize().toString('hex');
-  console.log(txToSign);
-
-  return txToSign;
+  // console.log(rawTx);
+  const txHex = ethers.utils.serializeTransaction(rawTx);
+  // console.log(txHex);
+  return {rawTx, txHex};
 }
 
 async function transfer() {
-  const provider = ethers.getDefaultProvider(); // mainnet infura provider
+  const chainId = 4; // rinkeby
+  const keyPath = "44'/60'/0'/0/0"; // ledger wallet key path
+  const provider = ethers.getDefaultProvider(NETWORKS[chainId]); // infura public provider
+  // const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL); // or pass provider custom URL here
+
   const transport = await TransportNodeHid.create();
-  console.log(transport);
+  // console.log(transport);
 
   const eth = new Eth(transport);
-  const keyPath = "44'/60'/0'/0/0";
-  const from = '0x1b6e35b91cBF70098D22757435Cbf6151B5C061B';
+  const {address: from} = await eth.getAddress(keyPath);
+  console.log(`from: ${from}`);
+
   const to = '0x2A0d2127173809f4E63517D71e8083Fe6a1410eD';
-  const usdtAmount = 100; // 100 usdt
+  const usdtAmount = 10; // 10 usdt
   const gasGwei = 10; // 10gwei gas price
-  const chainId = 4; // rinkeby
   const nonce = await provider.getTransactionCount(from);
-  const rawTx = getRawTx(
+  const {rawTx, txHex} = getRawTx(
     to,
     new BN(usdtAmount).times(1e6).toString(), // usdt to 6 decimals
     nonce,
     gasGwei,
     chainId
   );
-  const sig = await eth.signTransaction(keyPath, rawTx);
-
-  const signedTx = Object.assign({}, rawTx, {
-    r: sig.r,
-    s: sig.s,
-    v: sig.v
+  const {r, s, v} = await eth.signTransaction(keyPath, txHex.slice(2));
+  const signedTx = ethers.utils.serializeTransaction(rawTx, {
+    r: `0x${r}`,
+    s: `0x${s}`,
+    v: `0x${v}`
   });
+
+  // broadcast tx to network
   const tx = await provider.sendTransaction(signedTx);
-  return tx;
+  console.log(`tx sent, check with: https://rinkeby.etherscan.io/tx/${tx.hash}`);
+
+  // wait for tx confirm
+  console.log('now wait for confirm...');
+  const reciept = await tx.wait();
+  console.log('tx confirmed!');
+
+  return reciept;
 }
 
 transfer().then(tx => console.log(tx)).catch(e => console.log(e));
